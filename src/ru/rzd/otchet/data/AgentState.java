7059889608da996ffc7e52a555213b81567f901c
ATonevitskiy <ATonevitskiy@ATonevitskiy.serw.oao.rzd>
2015-10-13 12:22:53 +0200

    новый шаблон

---
+++ src/ru/rzd/otchet/data/AgentState.java
@@ -0,0 +1,18 @@
+package ru.rzd.otchet.data;
+
+/**
+ * Состояние 
+ * @author ATonevitskiy
+ */
+public enum AgentState {
+
+    NoName,
+    LogIn,
+    NotReady,
+    Ready,
+    Reserved,
+    Talking,
+    Work,
+    LogOut;
+
+}
---
+++ src/ru/rzd/otchet/data/Statist30min.java
@@ -0,0 +1,13 @@
+package ru.rzd.otchet.data;
+
+/**
+ *
+ * @author ATonevitskiy
+ */
+public class Statist30min {
+
+    private int startState;
+    private long workTime;
+    private int changeIn30Min;
+
+}
